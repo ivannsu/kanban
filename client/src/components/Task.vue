6cd98979fb29ad5f9997a64b45a7815d9b4ae540
ivan <ivansu.webmail@gmail.com>
2018-09-23 15:41:02 +0700

    task component

---
+++ client/src/components/Task.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card-body">
+    <h5 class="card-title">{{ task.title }}</h5>
+    <p class="card-text"><strong>Point</strong> : {{ task.point }}</p>
+    <p class="card-text"><strong>Assigned To</strong> : {{ task.assign }}</p>
+    <button class="btn btn-block btn-primary" data-toggle="modal" :data-target="modalTarget" @click="showDetail">Show Detail</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Task',
+  props: ['modalId', 'task', 'i'],
+  methods: {
+    showDetail () {
+      this.$emit('show-detail', { id: this.i, task: this.task })
+    }
+  },
+  computed: {
+    modalTarget () {
+      return `#${this.modalId}`
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
